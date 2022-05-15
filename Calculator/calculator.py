dd52d0be348d9e6ad6301c2fd6015a6d340f132d
Timothy Crundall <u5018130@anu.edu.au>
2022-05-15 20:55:29 +0200

    Calculator finished, flake8 configured

--- Calculator/calculator.py
+++ Calculator/calculator.py
@@ -1,32 +1,93 @@
 import PySimpleGUI as sg
 
-def button(label):
-    return sg.Button(label, key='-%s-'%(label.upper()))
+B_SIZE = (4, 2)
+# WIDE_BUTTON_SIZE = (10, 3)
 
-layout = [
-    [sg.Text('', key='-DISPLAY-')],
-    [sg.Button('Clear', key='-CLEAR-'), sg.Button('Equals', key='-EQUALS-')],
-    [button(label) for label in '123+'],
-    [button(label) for label in '456-'],
-    [button(label) for label in '789*'],
-    [button(label) for label in '0./'],
-    # [sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('+')],
-    # [sg.Button('4'), sg.Button('5'), sg.Button('6'), sg.Button('-')],
-    # [sg.Button('7'), sg.Button('8'), sg.Button('9'), sg.Button('*')],
-    # [sg.Button('0'), sg.Button('.'), sg.Button('/')],
-]
 
-window = sg.Window('Calculator', layout)
+def button(label, size=None, expand_x=False):
+    key = '-%s-'%(label.upper())
+    if size:
+        return sg.Button(label, key=key, size=size)
+    else:
+        return sg.Button(label, key=key, expand_x=expand_x)
 
-num_events = {'-%s-'%d:str(d) for d in range(10)}
 
+def create_window(theme):
+    sg.theme(theme)
+    sg.set_options(font='Franklin 14', button_element_size=B_SIZE)
+    layout = [
+        [sg.Text(
+            '',
+            key='-DISPLAY-',
+            font='Franklin 26',
+            expand_x=True,
+            justification='right',
+            right_click_menu=theme_menu)],
+        [
+            button('Clear', size=None, expand_x=True),
+            button('Equals', size=None, expand_x=True)],
+        [button(label, size=B_SIZE) for label in '789+'],
+        [button(label, size=B_SIZE) for label in '456-'],
+        [button(label, size=B_SIZE) for label in '123*'],
+        (
+            [button('0', expand_x=True)]
+            + [button(label, size=B_SIZE) for label in './']
+        ),
+    ]
+
+    return sg.Window('Calculator', layout, finalize=True)
+
+
+num_events = {'-%s-'%d: str(d) for d in range(10)}
+num_events['-.-'] = '.'
+
+oper_events = {'-%s-'%s: s for s in '+-*/'}
+
+theme_menu = ['menu', ['dark', 'LightGrey1', 'DarkGrey8', 'random']]
+
+window = create_window(theme='dark')
+# theme_changed = False
+current_num = []
+full_operation = []
+display = ''
 while True:
     event, values = window.read()
+    # if theme_changed:
+    #     window['-DISPLAY-'].update(display)
+    #     theme_changed = False
+
     if event == sg.WIN_CLOSED:
         break
 
     if event in num_events:
+        current_num.append(num_events[event])
+        window['-DISPLAY-'].update(''.join(current_num))
+
+    if event in oper_events:
+        full_operation.append(''.join(current_num))
+        full_operation.append(oper_events[event])
+        current_num = []
+
+    if event == '-EQUALS-':
+        if not full_operation:
+            continue
+
+        full_operation.append(''.join(current_num))
+        result = eval(''.join(full_operation))
+        window['-DISPLAY-'].update(str(result))
+        current_num = []
+        full_operation = []
+
+    if event == '-CLEAR-':
+        current_num = []
+        full_operation = []
+        window['-DISPLAY-'].update('')
+
+    if event in theme_menu[1]:
         display = window['-DISPLAY-'].get()
-        window['-DISPLAY-'].update(display + num_events[event])
+        window.close()
+        window = create_window(theme=event)
+        current_num = []
+        full_operation = []
 
 window.close()
